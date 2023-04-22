27565b1872a8248ff845e84c5ebd7107d71de2e5
KaidouoftheBeasts <dgomeztom@uoc.edu>
2023-04-22 18:47:59 +0200

    Cambios index.php

--- www/index.php
+++ www/index.php
@@ -6,75 +6,53 @@
     <head>
         <title>Welcome to LAMP Infrastructure</title>
         <meta charset="utf-8">
-        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
         <link rel="stylesheet" href="index.css">
     </head>
     <body>
-        <div class="header">
-            <div>
-                <h3 style="color:cadetblue;"> <strong>Grupo PSMD </strong></h3>
-                <h4 class="nombre-proyecto"> Gestión de Eventos </h4>
-            </div>
-            
-            <div class="botones"> 
-                <button class="boton registro"> Registro </button> 
-                <button class="boton acceso" > Acceso </button> 
-            </div>
-        </div>
-        <div class="cuerpo">
-            <div class="superior-cuerpo">
-                <div class="conferencias">
-                    <h3 style="color:rgb(31, 36, 171);"> Conferencias de tu especialidad con los mejores profesionales a escala global. Idiomas: español, inglés, alemán, chino y árabe. </h3>
-                    <img class="imagen" src="../assets/conferencia.jpg"> 
+        <div class="container-fluid">
+            <div class="row align-items-center header">
+                <div class="col">
+                    <h3 class="text-primary"> <strong>Grupo PSMD </strong></h3>
+                    <h4 class="nombre-proyecto"> Gestión de Eventos </h4>
                 </div>
-                <div class="seminarios">
-                    <h3 style="color:darkorange;"> Los mejores seminarios de tu especialidad 100% online. Se un profesional imparable. </h3>
-                    <img class="imagen" src="../assets/seminario.jpg"> 
-                </div>
-                <div class="mesa-redonda">
-                    <h3 style="color:rgb(11, 151, 11);"> Participa en alguna mesa redonda de debate presencialmente o desde casa.</h3>
-                    <img class="imagen" src="../assets/mesa-redonda.jpg"> 
+                <div class="col-auto d-flex">
+                    <button class="btn btn-primary registro"> Registro </button> 
+                    <button class="btn btn-primary acceso ml-2"> Acceso </button> 
                 </div>
             </div>
-            <div>
-                <h1 style="color:brown;"> <strong>Regístrate ya para poder participar en todos estos eventos que harán que despegues en tu carrera profesional </strong></h1>
+            <div class="row image-row">
+                <div class="col-lg-4 conferencias image-container">
+                    <div class="content-wrapper">
+                        <h3 class="text-info text-justified text-aligned"> Conferencias de tu especialidad con los mejores profesionales a escala global. Idiomas: español, inglés, alemán, chino y árabe. </h3>
+                        <img class="img-fluid" src="/assets/conferencia.jpg"> 
+                    </div>
+                </div>
+                <div class="col-lg-4 seminarios image-container">
+                    <div class="content-wrapper">
+                        <h3 class="text-warning text-justified text-aligned"> Los mejores seminarios de tu especialidad 100% online. Se un profesional imparable. </h3>
+                        <img class="img-fluid" src="/assets/seminario.jpg"> 
+                    </div>
+                </div>
+                <div class="col-lg-4 mesa-redonda image-container">
+                    <div class="content-wrapper">
+                        <h3 class="text-success text-justified text-aligned"> Participa en alguna mesa redonda de debate presencialmente o desde casa.</h3>
+                        <img class="img-fluid" src="/assets/mesa-redonda.jpg"> 
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <h1 class="text-danger"> <strong>Regístrate ya para poder participar en todos estos eventos que harán que despegues en tu carrera profesional </strong></h1>
+                </div>
             </div>
         </div>
         <div class="footer">
-
         </div>
-    
-        <!-- 
         <div class="container-fluid">
-            <?php
-                echo "<h1>¡Hola, grupo PSMD te da la bienvenida!</h1>";
-
-                //$conn = mysqli_connect('db', 'root', 'test', "dbname");
-                
-                $conn = mysqli_connect('db', 'root', 'test', "Eventos");
-
-                /*$query = 'SELECT * From Person';
-                $result = mysqli_query($conn, $query);
-
-                echo '<table class="table table-striped">';
-                echo '<thead><tr><th></th><th>id</th><th>name</th></tr></thead>';
-                while($value = $result->fetch_array(MYSQLI_ASSOC)){
-                    echo '<tr>';
-                    echo '<td><a href="#"><span class="glyphicon glyphicon-search"></span></a></td>';
-                    foreach($value as $element){
-                        echo '<td>' . $element . '</td>';
-                    }
-
-                    echo '</tr>';
-                }
-                echo
-                '</table>';
-
-                $result->close();*/
-                mysqli_close($conn);
-            ?>
         </div>
-        -->
     </body>
 </html>
--- www/login.php
+++ www/login.php
@@ -1,17 +1,41 @@
 <?php
   require_once 'db_connection.php';
 ?>
-    <!DOCTYPE html>
-    <html>
-        <head>
-            <title>Iniciar sesión - Gestión de Eventos</title>
-            <meta charset="utf-8">
-            <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-            <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-            <link rel="stylesheet" href="login.css">
-        </head>
-        <body>
-            <div class="container">
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Iniciar sesión - Gestión de Eventos</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="login.css">
+        <style>
+            body {
+                background-color: #f8f9fa;
+            }
+            .login-container {
+                max-width: 400px;
+                margin: 5% auto;
+                padding: 30px;
+                background-color: #ffffff;
+                border-radius: 5px;
+                box-shadow: 0 2px 5px rgba(0, 0, 0, 0.15);
+            }
+            h2 {
+                text-align: center;
+                margin-bottom: 30px;
+            }
+            button {
+                width: 100%;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="login-container">
                 <h2>Iniciar sesión</h2>
                 <form action="login_process.php" method="post">
                     <div class="form-group">
@@ -22,8 +46,9 @@
                         <label for="password">Contraseña:</label>
                         <input type="password" class="form-control" id="password" name="password" required>
                     </div>
-                    <button type="submit" class="btn btn-default">Iniciar sesión</button>
+                    <button type="submit" class="btn btn-primary">Iniciar sesión</button>
                 </form>
             </div>
-        </body>
-    </html>
+        </div>
+    </body>
+</html>
