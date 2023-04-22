4ed7549f0e5a923c9470922fd19605f936c31a98
KaidouoftheBeasts <dgomeztom@uoc.edu>
2023-04-22 19:41:38 +0200

    Cambios en registro.php

--- www/login.php
+++ www/login.php
@@ -1,17 +1,34 @@
 <?php
   require_once 'db_connection.php';
 ?>
-    <!DOCTYPE html>
-    <html>
-        <head>
-            <title>Iniciar sesión - Gestión de Eventos</title>
-            <meta charset="utf-8">
-            <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-            <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-            <link rel="stylesheet" href="login.css">
-        </head>
-        <body>
-            <div class="container">
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Iniciar sesión - Gestión de Eventos</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="index.css">
+        <link rel="stylesheet" href="login.css">
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row align-items-center header">
+                <div class="col">
+                    <h3 class="text-primary"> <strong>Grupo PSMD </strong></h3>
+                    <h4 class="nombre-proyecto"> Gestión de Eventos </h4>
+                </div>
+                <div class="col-auto d-flex">
+                    <button class="btn btn-primary registro" onclick="location.href='registro.php'"> Registro </button> 
+                    <button class="btn btn-primary volver ml-2" onclick="location.href='index.php'"> Volver </button> 
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="login-container">
                 <h2>Iniciar sesión</h2>
                 <form action="login_process.php" method="post">
                     <div class="form-group">
@@ -22,8 +39,9 @@
                         <label for="password">Contraseña:</label>
                         <input type="password" class="form-control" id="password" name="password" required>
                     </div>
-                    <button type="submit" class="btn btn-default">Iniciar sesión</button>
+                    <button type="submit" class="btn btn-primary">Iniciar sesión</button>
                 </form>
             </div>
-        </body>
-    </html>
+        </div>
+    </body>
+</html>
--- www/registro.php
+++ www/registro.php
@@ -1,29 +1,40 @@
 <?php
 require_once 'db_connection.php';
 ?>
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Registrate</title>
-        <link rel="stylesheet" href="index.css">
-    </head>
-    <body>
-        <div class="header">
-            <div>
-                <h3 style="color:cadetblue;"> <strong>Grupo PSMD </strong></h3>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registrate</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row align-items-center header">
+            <div class="col">
+                <h3 class="text-primary"> <strong>Grupo PSMD </strong></h3>
                 <h4 class="nombre-proyecto"> Gestión de Eventos </h4>
             </div>
-            
-            <div class="botones"> 
-                <button class="boton registro"> Registro </button> 
-                <button class="boton acceso"> Acceso </button> 
+            <div class="col-auto d-flex">
+                <button class="btn btn-primary acceso" onclick="location.href='login.php'"> Acceso </button>
+                <button class="btn btn-primary volver" onclick="location.href='index.php'"> Volver </button>
             </div>
         </div>
-        <div class="contenedor-registro">
-            
+        <div class="row">
+            <div class="col">
+                <div class="contenedor-registro">
+                    <!-- Contenido del registro aquí -->
+                </div>
+            </div>
         </div>
-    </body>
-    </html>
+    </div>
+    <div class="footer">
+    </div>
+    <div class="container-fluid">
+    </div>
+</body>
+</html>
