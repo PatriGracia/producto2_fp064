7ed90affb24bca4de7c599d4c29e2a462cebf775
PatriGracia <pgraciaj@uoc.edu>
2023-04-29 16:56:39 +0200

    Funcion inscribir/desinscribir done!

--- www/menus/usuario/infoActo.php
+++ www/menus/usuario/infoActo.php
@@ -1,5 +1,10 @@
 <?php
-
+session_start();
+if (!isset($_SESSION['user_id'])) {
+    header('Location: login.php');
+    exit;
+}
+$user_id = $_SESSION['user_id'];
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/db_connection.php';
 $conn = conexion();
@@ -7,16 +12,27 @@ $conn = conexion();
 $id = $_GET['info'];
 if (isset($_GET['info'])) {
     $id = $_GET['info'];
+
+    //Query para mostrar la informacion del acto seleccionado
     $sql = "SELECT Id_acto as id, Fecha as fecha, Hora as hora, Titulo as titulo, Descripcion_corta as descripcion_corta,
     Descripcion_larga as descripcion_larga, Num_asistentes as num_asistentes FROM Actos WHERE Id_acto = $id";
 
     $result = $conn->query($sql); 
 
-    //SELECT p.Nombre, p.Apellido1, p.Apellido2 FROM Personas p WHERE p.Id_persona IN (SELECT l.Id_persona FROM Lista_Ponentes l, Actos a WHERE l.Id_acto = 7)
-
+    //Query para mostrar lista de ponentes
     $sql2 = "SELECT p.Nombre as nombre, p.Apellido1 as apellido1, p.Apellido2 as apellido2 FROM Personas p WHERE p.Id_persona IN (SELECT l.Id_persona FROM Lista_Ponentes l, Actos a WHERE l.Id_acto = $id);";
 
     $result2 = $conn->query($sql2);
+
+    //Query comprobar inscrito en acto
+    $sql3 = mysqli_query($conn, "SELECT Id_acto FROM Actos WHERE Id_acto IN (SELECT b.id_acto FROM Inscritos b, Personas c WHERE b.Id_persona = $user_id);");
+    $result3 = mysqli_fetch_assoc($sql3);
+    if(isset($result3['Id_acto'])){
+        $id_query = $result3["Id_acto"];
+    }else{
+        $id_query = NULL;
+    };
+    
 
 }else{
     http_response_code(400);
@@ -37,7 +53,41 @@ if (isset($_GET['info'])) {
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body>
-    <div class="container-fluid">
+    <script> 
+        function inscribir(info){
+            const id_a = <?php echo $id; ?>;
+            $.ajax({
+                    url: 'inscribirActo.php',
+                    method: 'POST',
+                    data: { id: id_a },
+                    dataType: 'json',
+                    success: function (msg) {
+                        alert('Te has inscrito correctamente.');
+                    },
+                    error: function () {
+                        alert('Error al inscribir.');
+                    }
+                });
+        };
+        function desinscribir(){
+            const id_a = <?php echo $id; ?>;
+            $.ajax({
+                    url: 'desinscribirActo.php',
+                    method: 'POST',
+                    data: { id: id_a },
+                    dataType: 'json',
+                    success: function (msg) {
+                        alert('Ya no estás inscrito en este acto');
+                    },
+                    error: function () {
+                        alert('Error al desinscribir.');
+                    }
+                });
+        };
+
+    </script>        
+
+    <div class="container-fluid" id="InfoCompletaActo">
         <div class="row align-items-center header">
             <div class="col">
                 <h3 class="text-primary"> <strong>Grupo PSMD </strong></h3>
@@ -52,7 +102,6 @@ if (isset($_GET['info'])) {
             <h1 class="text-center">Información del acto</h1>
             <div class="row">
             <div class="col">
-                <!-- <h2 class="text-center">Lista de Ponentes</h2> -->
                 <table class="table table-bordered table-striped">
                     <thead>
                         <tr>
@@ -70,7 +119,7 @@ if (isset($_GET['info'])) {
                         if ($result->num_rows > 0) {
                             while($row = $result->fetch_assoc()) {
                                 echo "<tr>";
-                                echo "<td>" . $row["id"] . "</td>";
+                                echo "<td id='id_a'>" . $row["id"] . "</td>";
                                 echo "<td>" . $row["titulo"] . "</td>";
                                 echo "<td>" . $row["fecha"] . "</td>";
                                 echo "<td>" . $row["hora"] . "</td>";
@@ -98,16 +147,24 @@ if (isset($_GET['info'])) {
                         } else {
                             echo "<tr><td colspan='5'>No se encontraron ponentes</td></tr>";
                         }
-                        ?>
+                    ?>
                     </ul>
                 </div>
             </div>
         </div>
+        <div class="footer">
+            <?php
+                if ($id != $id_query) {
+            ?>
+                    <button type='button' class='btn btn-outline-secondary' id="BotonInscribir" onclick="inscribir()">Inscribirse</button>
+            <?php       
+                } else {
+            ?>
+                    <button type='button' class='btn btn-outline-secondary' id='BotonDesinscribir' onclick="desinscribir()">Desinscribirse</button>
+            <?php
+                }
+            ?>
+        </div>
     </div>
 </body>
 </html>
-
-
-
-
-   
